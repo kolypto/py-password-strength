bf557d2f8a1a703c36d1895cff62c0980ef80131
Mark Vartanyan <kolypto@gmail.com>
2014-08-03 00:50:43 +0200

    package

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://api.travis-ci.org/kolypto/py-password-policy.png?branch=master)](https://travis-ci.org/kolypto/py-password-policy)
+
+
+Password Policy
+===============
+
+Check if a password meets the security policy.
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+""" Password policy checker """
+
+from setuptools import setup, find_packages
+
+setup(
+    name='password_policy',
+    version='0.0.1-0',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-password-policy',
+    license='BSD',
+    description=__doc__,
+    long_description=open('README.rst').read(),
+    keywords=['password', 'strength', 'policy', 'security'],
+
+    packages=find_packages(),
+    scripts=[],
+    entry_points={},
+
+    install_requires=[
+    ],
+    extras_require={
+        '_tests': ['nose', ],
+    },
+    include_package_data=True,
+    test_suite='nose.collector',
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
